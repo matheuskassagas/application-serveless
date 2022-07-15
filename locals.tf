08f79206d006a5c215eddf8fcdc6f848d198bf5d
matheuskassagas <gigliotti_@hotmail.com>
2022-07-15 14:37:40 -0300

    variables created

---
+++ locals.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  type        = string
+  description = ""
+  default     = "eu-east-1"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = ""
+  default     = "default"
+}
+
+variable "aws_account_id" {
+  type        = number
+  description = ""
+  default     = 000000000000
+}
+
+variable "service_name" {
+  type        = string
+  description = ""
+  default     = "To-dos"
+}
+
+variable "service_domain" {
+  type        = string
+  description = ""
+  default     = "api-to-dos"
+}
